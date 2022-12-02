54f6650d0e2b32f0fc8c3b7ea37cf08b933b3caa
Laura <januleviciute.l@gmail.com>
2022-12-02 12:09:26 +0200

    uzd

--- uzduotys.js
+++ uzduotys.js
@@ -12,6 +12,8 @@
 // 10. Parašyti funkciją telefonoNumeris.Funkcija turi priimti vieną kintamąjį - masyvą.Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu -
 //     "(XXX) XXX-XXXX". (10 taškų)
 
+console.log('-------- Užd. Nr. 1------------')
+
 // Užd. Nr. 1:
 // 1. Palyginti du skaičius a ir b.Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
 const a = 4;
@@ -24,7 +26,7 @@ if (a === b) {
     console.log('b daugiau už a');
 }
 
-console.log('-------------------------------------')
+console.log('-------- Užd. Nr. 2 -----------')
 
 // Užd. Nr. 2:
 // 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10.(5 taškai)
@@ -32,7 +34,7 @@ for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
 
-console.log('-------------------------------------')
+console.log('-------- Užd. Nr. 3 -----------')
 
 // Užd. Nr. 3:
 // 3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10.(5 taškai)
@@ -40,7 +42,7 @@ for (let i = 0; i <= 10; i = i + 2) {
     console.log(i);
 }
 
-console.log('-------------------------------------')
+console.log('-------- Užd. Nr. 4 -----------')
 
 // Užd. Nr. 4:
 // 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
